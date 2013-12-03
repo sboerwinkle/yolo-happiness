040bf4e52a9ecd7ff5ea5736eb68fcc30c04cffe
Simon Boerwinkle <sboerwinkle@gmail.com>
2013-12-03 17:37:06 -0600

    Actually added the modified main.cpp this time.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <SDL/SDL.h>
 
 using namespace std;
 
 int main(int argc, char** argv){
-	cout << "It works!" << endl;
-	int i = 1;
-	for(; i < argc; i++) cout << argv[i] << endl;
-	return 0;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Surface* screen = SDL_SetVideoMode(500, 500, 32, SDL_SWSURFACE);
+	SDL_Surface* oldImg = SDL_LoadBMP("img.bmp");
+	SDL_Surface* img = SDL_DisplayFormat(oldImg);
+	SDL_FreeSurface(oldImg);
+	SDL_BlitSurface(img, NULL, screen, NULL);
+	SDL_Event e;
+	SDL_Rect dest;
+	SDL_Rect wholeScreen;
+	wholeScreen.x = wholeScreen.y = 0;
+	wholeScreen.w = wholeScreen.h = 500;
+	dest.x = dest.y = 0;
+	while(true){
+		SDL_WaitEvent(&e);
+		if(e.type == SDL_QUIT){
+			SDL_FreeSurface(img);
+			SDL_Quit();
+			return 0;
+		}
+		if(e.type == SDL_KEYDOWN){
+			if(e.key.keysym.sym == SDLK_LEFT) dest.x--;
+			else if(e.key.keysym.sym == SDLK_RIGHT) dest.x++;
+			else if(e.key.keysym.sym == SDLK_UP) dest.y--;
+			else if(e.key.keysym.sym == SDLK_DOWN) dest.y++;
+		}
+
+		SDL_FillRect(screen, &wholeScreen, 0x000000FF);
+		SDL_BlitSurface(img, NULL, screen, &dest);
+		SDL_Flip(screen);
+	}
 }
