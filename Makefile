92c23bd78f60f75b98f75856f5234c532e220c21
Russell Bryan <bryanr@asmsa.org>
2013-12-04 14:37:49 -0600

    Pieces can now move added math.

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ LFLAGS = -lSDL
 
 .PHONY: clean
 
-game: main.o 
-	$(CC) $(CFLAGS) $(LFLAGS) main.o -o game
+game: main.o math.o 	
+	$(CC) $(CFLAGS) $(LFLAGS) math.o main.o -o game
 
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c main.cpp
+
+math.o: math.cpp
+	$(CC) $(CFLAGS) -c math.cpp
 	
 clean:
 	rm -rf ./*.o game
--- pieces.hpp
+++ pieces.hpp
@@ -14,66 +14,136 @@ enum piece_colour{
 };
 
 
+struct pt{
+
+	int x,y;
+
+};
+
+
 class Piece{
 
 	public:
 		piece_t type;
 		piece_colour colour;
+		pt pos;
+		
+		Piece newPiece(	int x, 
+				int y, 
+				piece_t type, 
+				piece_colour newColour);
 
-		Piece newPiece(piece_t type);	
 		piece_t getType();
 		piece_colour getColour();
+		void move(int x, int y);	
 };
 
 class Pawn: public Piece{
+	public:
+		Pawn(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
 
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
+		
 };
 
 class Rook: public Piece{
+	public:
+		Rook(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
+			
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
 
 };
 
 class Knight: public Piece{
+	public:
+		Knight(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
+		
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
 
+	
 };
 
 class Bishop: public Piece{
-
+	public:
+		Bishop(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
+		
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
+	
 };
 
 class King: public Piece{
+	public:
+		King(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
+		
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
 
 };
 
 class Queen: public Piece{
+	public:
+		Queen(	int x=0, 
+			int y=0, 
+			piece_colour c = BLACK){
+		
+			pos.x = x;
+			pos.y = y;
+			colour = c;	
+		}
 
 };
 
 
-Piece Piece::newPiece(	piece_t newType=PAWN,
+Piece Piece::newPiece(	int x, 
+			int y,	
+			piece_t newType=PAWN,
 	       		piece_colour newColour=BLACK){
 	
 	switch (newType){
 		case PAWN:
-			return Pawn();
+			return Pawn( x, y, newColour);
 			break;
 		case ROOK:
-			return Rook();
+			return Rook( x, y, newColour);
 			break;
 		case KNIGHT:
-			return Knight();
+			return Knight( x, y, newColour);
 			break;
 		case BISHOP:
-			return Bishop();
+			return Bishop( x, y, newColour);
 			break;
 		case KING:
-			return King();
+			return King( x, y, newColour);
 			break;
 		case QUEEN:
-			return Queen();
+			return Queen( x, y, newColour);
 			break;
 		default:
-			return Pawn();
+			return Pawn( x, y, newColour);
 			break;
 
 	}
@@ -87,3 +157,8 @@ piece_t Piece::getType(){
 piece_colour Piece::getColour(){
 	return colour;
 }
+void Piece::move(int x, int y){
+	pos.x = x;
+	pos.y = y;
+}
+
